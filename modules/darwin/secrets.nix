d1e7b00762187ba13e5715cc106e463142c3c1b2
aloshy.🅰🅸 <noreply@aloshy.ai>
2025-02-14 00:31:30 +0400

    Update GitHub SSH key path in secrets.nix

--- modules/darwin/secrets.nix
+++ modules/darwin/secrets.nix
@@ -17,14 +17,14 @@ let user = "aloshy"; in
   #       so Github reads it correctly.
 
   #
-  # age.secrets."github-ssh-key" = {
-  #   symlink = true;
-  #   path = "/Users/${user}/.ssh/id_github";
-  #   file =  "${secrets}/github-ssh-key.age";
-  #   mode = "600";
-  #   owner = "${user}";
-  #   group = "staff";
-  # };
+  age.secrets."github-ssh-key" = {
+    symlink = true;
+    path = "/Users/${user}/.ssh/id_ed25519";
+    file =  "${secrets}/github-ssh-key.age";
+    mode = "600";
+    owner = "${user}";
+    group = "staff";
+  };
 
   # age.secrets."github-signing-key" = {
   #   symlink = false;
