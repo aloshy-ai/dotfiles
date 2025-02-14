5507d2b3d13863ddfed2b757697ab5a80d693ed3
aloshy.🅰🅸 <noreply@aloshy.ai>
2025-02-14 14:42:44 +0400

    Remove user modules configuration from Polybar setup

--- modules/nixos/home-manager.nix
+++ modules/nixos/home-manager.nix
@@ -6,15 +6,6 @@ let
   shared-programs = import ../shared/home-manager.nix { inherit config pkgs lib; };
   shared-files = import ../shared/files.nix { inherit config pkgs; };
 
-  polybar-user_modules = builtins.readFile (pkgs.substituteAll {
-    src = ./config/polybar/user_modules.ini;
-    packages = "${xdg_configHome}/polybar/bin/check-nixos-updates.sh";
-    searchpkgs = "${xdg_configHome}/polybar/bin/search-nixos-updates.sh";
-    launcher = "${xdg_configHome}/polybar/bin/launcher.sh";
-    powermenu = "${xdg_configHome}/rofi/bin/powermenu.sh";
-    calendar = "${xdg_configHome}/polybar/bin/popup-calendar.sh";
-  });
-
   polybar-config = pkgs.substituteAll {
     src = ./config/polybar/config.ini;
     font0 = "DejaVu Sans:size=12;3";
@@ -63,7 +54,7 @@ in
     polybar = {
       enable = true;
       config = polybar-config;
-      extraConfig = polybar-bars + polybar-colors + polybar-modules + polybar-user_modules;
+      extraConfig = polybar-bars + polybar-colors + polybar-modules;
       package = pkgs.polybarFull;
       script = "polybar main &";
     };
